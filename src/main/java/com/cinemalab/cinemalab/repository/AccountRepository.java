69317edc0be33771f0b7415ec196cc50256aa112
sofiiaru <rudenka93@gmail.com>
2025-03-20 16:51:11 -0400

    jpqa queries and @Query annot

--- src/main/java/com/cinemalab/cinemalab/repository/AccountRepository.java
+++ src/main/java/com/cinemalab/cinemalab/repository/AccountRepository.java
@@ -1,13 +1,45 @@
 package com.cinemalab.cinemalab.repository;
 
 import com.cinemalab.cinemalab.entity.Account;
+import com.cinemalab.cinemalab.enums.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
     //method --> sql --> derive query    findById(int id)
 
     //sql - @Query
+    @Query(value = "SELECT * FROM accounts WHERE role = ?1", nativeQuery = true)
+    List<Account> readAccountsByRole(Role role);
 
     //JPQL - @Query
+    @Query("SELECT a FROM Account a WHERE a.id=?1")
+    Optional<Account> getAccountDetails(Long accountId);
+
+    //get account with email not equal
+    @Query("SELECT a FROM Account a WHERE a.email <> ?1")
+    Optional<Account> getAccountDetailsEmailNotEqual(String email);
+
+    //get accounts with role
+    @Query("SELECT a FROM Account a WHERE a.role = :role")
+    List<Account> getAccountsWithRole(@Param("role") Role role);
+
+
+    @Query("UPDATE Account a SET a.country = :country WHERE a.email = :email")
+    @Modifying
+    @Transactional
+    void updateAccountCountryWithEmail(@Param("email") String email, @Param("country") String country);
+
+    List<Account> retrieveAccountsByCountry(String country);
+
+
+
+
 }
--- src/main/java/com/cinemalab/cinemalab/repository/LocationRepository.java
+++ src/main/java/com/cinemalab/cinemalab/repository/LocationRepository.java
@@ -3,6 +3,7 @@ package com.cinemalab.cinemalab.repository;
 import com.cinemalab.cinemalab.entity.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface LocationRepository extends JpaRepository<Location, Long> {
@@ -22,6 +23,53 @@ public interface LocationRepository extends JpaRepository<Location, Long> {
 
     //display top 2 locations in canada
     List<Location> findTop2ByCountry(String country);
+
+    //findDistinctTop3ByDivisionContaining(String pattern)
+
+    //display all employees with email address "gmail.com"
+    //findByEmailEnding(String domain)
+
+    //display all employees with first name " " and last name " ", also show all employees with an email address
+    //findByFirstNameAndLastNameOrEmail(String firstName, String lastName, String email);
+
+    //display all employees that first name is not "Simon"
+    //findByFirstNameIsNot(String firstName)
+
+    //display all employees where last name starts with "Sim"
+    //findByLastNameStartsWith(String lastName)
+
+    //display all employees with salaries less than or equal '100 000'
+    //findBySalaryLessThanEqual(Integer salary)
+
+    //display all employees that has been hired between dates " " and " "
+    //findByHireDateBetween(LocalDate startDate, LocalDate endDate)
+
+    //display all employees who don't have email address
+    //findByEmailIsNull
+
+    //find all courses by category and Order entities by name
+    //findByCategoryOrderByName(String category)
+
+    //check if a course with provided name exists
+    //boolean existsByName(String name)
+
+    //return the count of courses for the provided category
+    //int countByCategory(String category)
+
+    //find all courses that start with provided course name
+    //findAllByNameStartsWith(String name)
+
+    //find all courses by category and returns a stream
+    //Stream<Course> streamByCategory(String category)
+
+    //
+
+
+
+
+
+
+
 
 
 
